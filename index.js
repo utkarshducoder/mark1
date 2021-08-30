8b729ce0c97347a3f9f60b2c5c72505fe4fd75f2
utkarshducoder <83503804+utkarshducoder@users.noreply.github.com>
2021-08-30 23:58:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,192 +1,110 @@
-  const chalk = require('chalk');
-  const log = console.log;
-  const readline = require('readline');
-  var score = 0;
-  var letterNumber =  /^[0-9a-zA-Z]+$/;
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+var readlineSync= require("readline-sync");
+var score = 0;
+const chalk = require('chalk');
 
-    log(chalk.green(
-	'Hello people Get ready for a small quiz on' +
-	chalk.underline.red.bold(' coronavirus ')
+let log = console.log
 
-));
+let blueBright = chalk.bold.blueBright
+let green = chalk.bold.green
+let red = chalk.bold.red
+let cyan = chalk.bold.cyan
+let yellow = chalk.bold.yellow 
+let title = chalk.black.bold.bgYellow
+let white = chalk.white.bold.underline
 
- 
-log(chalk.green('Only', chalk.underline.red.bold('small and capital letters ') + ' can be used for answering the questions\n\n'));
+log(blueBright('Hey guys let us know how much you know about', chalk.underline.bgGreenBright('Utkarsh Gupta!\n')));
 
-log(chalk.green('This quiz will let you know how much you know about', chalk.green.underline.bgRed('Covid-19\n')));
+var userName = readlineSync.question(log(red("may I have your name?" + " ")));
 
-
- log(chalk.green(
-	'I hope you will' +
-	chalk.red.bold(' enjoy!! ') + ''
-
-));
- 
-  //log("I hope you will enjoy this!!\n");
-
-  log(chalk.green(
-	'For a wrong answer you will get the' +
-	chalk.red.bold(' same question\n\n ')
-
-));
+log(green("hello" +" " + userName))
 
 
 
+log(blueBright('Keep in mind these intructions lets start with how much you know about',title ('Utkarsh Gupta!\n')))
 
+log(green('Here', 'is', 'the', 'first', 'question\n'));
 
-  const questionBankk = [
-  {
-      question: `
-    The first case of novel coronavirus was identified in .....
+var questionBank = [{
+  question: `The first case of novel coronavirus was identified in .....
     a: Beijing
     b: Shanghai
     c: Wuhan, Hubei
     d: Tianjin\n`,
-      answer: "c"
-    },
-    {
-      question: `
-    Which of the following diseases are related to coronavirus?
+      answer: "c"},
+{
+  question: `Which of the following diseases are related to coronavirus?
     a: MERS
     b: SARS
     c: Both A and B
     d: Neither A nor B\n`,
-      answer: "c"
-    },
-    {
-      question: `
-    In India, when did the second phase of COVID-19 vaccination start?
+      answer: "c"},
+
+
+{
+  question: `In India, when did the second phase of COVID-19 vaccination start?
     a: December 2020
     b: January 2021
     c: February 2021
     d: March 2021\n`,
-      answer: "d"
-    },
-    {
-        question: `
-    Name a clinical trial in which blood is transfused from recovered COVID-19 patients to a coronavirus patient who is in critical condition?
+      answer: "d"},
+{
+  question: `Name a clinical trial in which blood is transfused from recovered COVID-19 patients to a coronavirus patient who is in critical condition?
     a: Plasma Therapy
     b: Hydroxychloroquine
     c: Remdesivir
     d: Solidarity\n`,
-      answer: "a"
-    },
-    {
-        question: `
-    When was the COVID-19 vaccine launched in India?
+      answer: "a"},
+ {
+   question: `When was the COVID-19 vaccine launched in India?
     a: 25th December, 2020
     b: 16th January, 2021
     c: 1st February, 2021
     d: 12th March, 2021\n`,
-      answer: "b"
-    },
-    {
-      question: `
-    During covid-19 which state suffered most in India?
+      answer: "b"},
+ {
+   question: `During covid-19 which state suffered most in India?
     a: Rajasthan
     b: Kerala
     c: Maharashtra
     d: Karnataka\n`,
-      answer: "c"
-    }
-  ];
+      answer: "c"},
+];
 
-  let qkI = 0;
 
-  function question(){
-    rl.question(questionBankk[qkI].question, (answer) => {
-      
-
-      
-      //console.log(`You answered: ${answer}`);
-
-      if(answer.toLowerCase() == questionBankk[qkI].answer.toLowerCase()) {
-       
-       log(chalk.green('right' , 'answer'));
-       
-       //console.log("right answer");
-        qkI++;
-        score = score+1;
-        console.log(score);
-        
-        
-        serve();
-      }
-      else{
-
-        log(chalk.red('wrong' , 'answer'));
-        //console.log("wrong answer");
  
-      log(chalk.green('Answers only varies from', chalk.red.bold('a to d either in capital or small\n\n ')));
 
-        //console.log("Answers only varies from a to d either in capital or in small");
-        question();
-      }
-    });
-  }
-  function serve()
-  {
-    if(isQuestionBankExhausted()) {
-      
-      log(chalk.green('Thanks for playing and spending your valuable time'));
-      
-      //console.log("Thanks for playing and spending your valuable time");
-        
-               log(chalk.blue('I hope you enjoy this game'));
 
-         log(chalk.green('Your', chalk.red(' final score ') + 'is'));
-      
+function play(question,answer){
+ 
+userAnswer = readlineSync.question(red(question+"\n"));
 
-      //console.log("Your final score is");
-      console.log(score);
-      rl.close();
-    }
-    else{
-      
-     
-      
-      
-      
-     rl.question("What do you want to do? \nto play then press any key, want to quit then press e", (choice) => {
-       
-        log(chalk.red('You', 'selected'));
+if(userAnswer.toUpperCase() === answer.toUpperCase()){
+console.log("you were right!");
+score = score + 4;
 
-       console.log(`${choice}`);
+}
+else{
+  console.log("you were wrong");
+ 
+  score = score - 2;
+}
+console.log("your score is :", score);
 
-        if( choice.toLowerCase() !=="e"){
-          question();
-          
-        }
-        else{
-          
-          log(chalk.green('Thanks for playing and spending your valuable time'));
-      
-          
-          //console.log("Thanks for playing");
-           log(chalk.blue('I hope you enjoy this game'));
-          //log("I hope you enjoy this game");
-        
-        log(chalk.green('Your', chalk.red(' final score ') + 'is'));
-        
-        //log("Your final score is");
-        console.log(score);
-          rl.close();
-        }
-      });
-    }
-  }
 
-  function isQuestionBankExhausted() {
-    if (questionBankk.length == qkI) {
-      return true;
-    }
-  }
-
-  serve();
+} 
 
 
 
+
+
+for( var i = 0; i<questionBank.length; i++)
+{
+ var cur= questionBank[i];
+ play(cur.question,cur.answer);
+ console.log(" ");
+ console.log(chalk.yellow("*-*-*-*-*-*-*-*-*-*-*-*-*-*"));
+
+}
+
+log(cyan("THE END"))
+log(green("Thank you for spending your valuable time"), red("I hope you like this game"))
